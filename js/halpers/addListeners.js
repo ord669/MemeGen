c6522aae1c9c7419846ae92ae66503308cbd9849
ord669 <ord24@gmail.com>
2022-12-14 16:20:50 +0700

    works with event listieners

---
+++ js/halpers/addListeners.js
@@ -0,0 +1,26 @@
+'use strict'
+
+
+//Handle the listeners
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+    //Listen for resize ev
+    window.addEventListener('resize', () => {
+        resizeCanvas()
+        renderCanvas()
+
+    })
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchend', onUp)
+}
--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,1 +1,95 @@
 'use strict'
+
+let gElCanvas
+let gCtx
+let gStartPos
+const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
+
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    resizeCanvas()
+    //Calc the center of the canvas
+    const center = { x: gElCanvas.width / 2, y: gElCanvas.height / 2 }
+    //Create the circle in the center
+    // createCircle(center)
+
+    addListeners()
+    renderCanvas()
+}
+
+
+function renderCanvas() {
+    //Set the backgournd color to grey
+    gCtx.fillStyle = '#ede5ff'
+    //Clear the canvas,  fill it with grey background
+    gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+    // renderCircle()
+}
+
+
+
+function onDown(ev) {
+    // Get the ev pos from mouse or touch
+    const pos = getEvPos(ev)
+    // if (!isCircleClicked(pos)) return
+
+    // setCircleDrag(true)
+    //Save the pos we start from
+    gStartPos = pos
+    document.body.style.cursor = 'grabbing'
+}
+
+function onMove(ev) {
+    // const { isDrag } = getCircle()
+
+    // if (!isDrag) return
+    
+    const pos = getEvPos(ev)
+    console.log('pos:', pos)
+    // Calc the delta , the diff we moved
+    const dx = pos.x - gStartPos.x
+    const dy = pos.y - gStartPos.y
+    // moveCircle(dx, dy)
+    // Save the last pos , we remember where we`ve been and move accordingly
+    gStartPos = pos
+    // The canvas is render again after every move
+    renderCanvas()
+}
+
+function onUp() {
+    // setCircleDrag(false)
+    document.body.style.cursor = 'grab'
+}
+
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+}
+
+
+function getEvPos(ev) {
+    // Gets the offset pos , the default pos
+    let pos = {
+        x: ev.offsetX,
+        y: ev.offsetY,
+    }
+    // Check if its a touch ev
+    if (TOUCH_EVS.includes(ev.type)) {
+        console.log('ev:', ev)
+        //soo we will not trigger the mouse ev
+        ev.preventDefault()
+        //Gets the first touch point
+        ev = ev.changedTouches[0]
+        //Calc the right pos according to the touch screen
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+        }
+    }
+    return pos
+}
